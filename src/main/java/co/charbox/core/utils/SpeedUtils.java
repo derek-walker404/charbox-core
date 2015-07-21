08a9d9be4e093caa1379f7e77fa5835f21e6b647
David Esposito <desposi1@gmail.com>
2015-07-21 02:43:33 -0400

    'Convert to new auth/permissions. Fixed SST speed calculation.'

--- src/main/java/co/charbox/core/utils/SpeedUtils.java
+++ src/main/java/co/charbox/core/utils/SpeedUtils.java
@@ -3,10 +3,18 @@ package co.charbox.core.utils;
 
 public abstract class SpeedUtils {
 
+	// to kb 1024
+	// to mb 1024
+	// to seconds 1000
+	// to bits 8
+	// const = 8  * 1000 / 1024 / 1024 = 0.008
+	//         bits sec    kb     mb
+	private static final double CONST = 8.0 * 1000.0 / 1024.0 / 1024.0;
+	
 	public static double calcSpeed(double duration, double size) {
 		double speed = (duration <= 0) 
-				? -1 
-				: (size * 8.0 / 1048.576 / duration); // do I need to multiple by 8 here? math don't work now. comm bak laytur, da nomburs luk gud doh
+				? -1
+				: (size * CONST / duration);
 		return speed;
 	}
 }
